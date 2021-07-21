fd997724f2f7e74d437a538c7a3bd3c69f3702a5
suhas <suhaspd@gmail.com>
2021-07-21 14:49:51 +0530

    shared libraries

---
+++ vars/desgUser.groovy
@@ -0,0 +1,18 @@
+def call(String username = 'null', String designation = 'null') {
+	echo "This is shared library sendoffUser: ${username}"
+	pipeline {
+		environment {
+			USER_NAME = "$username"
+			DESG = "${designation}"
+		}
+		agent any 
+		stages {
+			stage('SENDOF_USER') {
+				steps {
+					echo "Bye $USER_NAME, Nice meeting you !"
+					echo "Designation: $DESG"
+				}	
+			}
+		}
+	}
+}
